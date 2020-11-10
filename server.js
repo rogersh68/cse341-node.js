fa120fddf38a108cc5ec6b33c41901f6f2bdd9ec
Hannah Rogers <rog19008@byui.edu>
2020-11-10 09:37:10 -0700

    testing postal rate calc

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('public/form'))
   .get('/getRate', getRate)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
