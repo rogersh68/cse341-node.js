8f87d0a6d416442848d80f5a471730f6af7257b5
Hannah Rogers <rog19008@byui.edu>
2020-12-04 17:44:48 -0700

    testing

--- models/clothing-model.js
+++ models/clothing-model.js
@@ -1,102 +1,148 @@
 const connection = require("../library/connection.js");
 
 function getClothesByUserIdAndWarmRating(userId, warmRating, callback) {
+	// set up sql for query
 	let sql = "SELECT * FROM clothing WHERE userid = $1 AND warmrating = $2";
+
+	// set up sql parameters
 	let params = [userId, warmRating];
+
+	// run query, send results to callback function
 	connection.pool.query(sql, params, (error, result) => {
 		if (error) {
 			console.log("--> clothing-model.js > getClothesByUserIdAndWarmRating");
 			console.log(error);
+
+			// send back error
 			callback(error, null);
 		}
 		console.log("--> clothing-model.js > getClothesByUserIdAndWarmRating");
 		console.log(result.rows);
+
+		// send back results
 		callback(null, result.rows);
-	})
+	});
 }
 
 function getClothesByUserId(userId, callback) {
+	// set up sql for query
 	let sql = "SELECT * FROM clothing WHERE userid = $1";
+
+	// set up sql parameters
 	let params = [userId];
+
+	// run query, send results to callback function
 	connection.pool.query(sql, params, (error, result) => {
 		if(error) {
 			console.log("--> clothing-model.js > getClothesByUserId");
 			console.log(error);
+
+			// send back error
 			callback(error, null);
 		}
 		console.log("--> clothing-model.js > getClothesByUserId");
 		console.log(result.rows);
+
+		// send back results
 		callback(null, result.rows);
 	});
 }
 
 function getClothingByClothingId(clothingId, callback) {
+	// set up sql for query
 	let sql = "SELECT * FROM clothing WHERE clothingid = $1";
+
+	// set up sql parameters
 	let params = [clothingId];
+
+	// run query, send results to callback function
 	connection.pool.query(sql, params, (error, result) => {
 		if (error) {
 			console.log("--> clothing-model.js > getClothingByClothingId");
 			console.log(error);
+
+			// send back error
 			callback(error, null);
 		}
 		console.log("--> clothing-model.js > getClothingByClothingId");
 		console.log(result.rows[0]);
+
+		// send back results
 		callback(null, result.rows[0]);
 	})
 }
 
 function insertClothingItem(clothingType, clothingColor, warmRating, casualRating, clothingImage, userId, callback) {
-	let sql = "INSERT INTO clothing (clothingtype, clothingcolor, warmrating, casualrating, clothingimage, userid) VALUES ($1, $2, $3, $4, $5, $6)"
+	// set up sql for query
+	let sql = "INSERT INTO clothing (clothingtype, clothingcolor, warmrating, casualrating, clothingimage, userid) VALUES ($1, $2, $3, $4, $5, $6)";
+	
+	// set up sql parameters
 	let params = [clothingType, clothingColor, warmRating, casualRating, clothingImage, userId];
+	
+	// run query, send results to callback function
 	connection.pool.query(sql, params, (error, result) => {
 		if (error) {
 			console.log("--> clothing-model.js > insertClothingItem");
 			console.log(error);
-			callback(error, null);
-		}
-		db_result = {
-			success: true,
-			list: result.rows
+
+			// send back error and success = false
+			callback(error, {success: false});
 		}
 		console.log("--> clothing-model.js > insertClothingItem");
-		console.log(db_result)
-		callback(null, db_result)
+		console.log(result);
+
+		// send back success = true
+		callback(null, {success: true});
 	});
 }
 
 function updateClothingItemInDb(clothingId, clothingType, clothingColor, warmRating, casualRating, clothingImage, callback) {
+	// set up sql for query
 	let sql = "UPDATE clothing SET clothingtype = $1, clothingcolor = $2, warmrating = $3, casualrating = $4, clothingimage = $5 WHERE clothingid = $6";
+	
+	// set up sql parameters
 	let params = [clothingType, clothingColor, warmRating, casualRating, clothingImage, clothingId];
+	
+	// run query, send results to callback function
 	connection.pool.query(sql, params, (error, result) => {
 		if (error) {
 			console.log("--> clothing-model.js > updateClothingItemInDb");
 			console.log(error);
-			callback(error, null);
+
+			// send back error and success = false
+			callback(error, {success: false});
 		}
-		db_result = {success:true, list: result.rows}
 		console.log("--> clothing-model.js > updateClothingItemInDb");
-		console.log(db_result)
-		callback(null, db_result);
+		console.log(result);
+
+		// send back success = true
+		callback(null, {success: true});
 	});
 }
 
 function deleteClothingItemInDb(clothingId, callback) {
+	// set up sql for query
 	let sql = "DELETE FROM clothing WHERE clothingid = $1";
+
+	// set up sql parameters
 	let params = [clothingId];
+
+	// run query, send results to callback function
 	connection.pool.query(sql, params, (error, result) => {
 		if (error) {
 			console.log("--> clothing-model.js > deleteClothingItemInDb");
 			console.log(error);
-			callback(error, null);
+
+			// send back error and success = false
+			callback(error, {success: false});
 		}
-		db_result = {success:true, list: result.rows}
 		console.log("--> clothing-model.js > deleteClothingItemInDb");
-		console.log(db_result)
-		callback(null, db_result);
+		console.log(result);
+
+		// send back success = true
+		callback(null, {success: true});
 	});
 }
-
-
 
 module.exports = {
 	getClothesByUserIdAndWarmRating: getClothesByUserIdAndWarmRating,
--- models/user-model.js
+++ models/user-model.js
@@ -1,39 +1,50 @@
 const connection = require("../library/connection.js");
 
 function getUserByEmail(userEmail, callback) {
-	// Set up SQL for query
+	// set up sql for query
 	let sql = "SELECT * FROM public.user WHERE useremail = $1";
 
-	// set up SQL parameters
-	let params = [userEmail]
+	// set up sql parameters
+	let params = [userEmail];
 
 	// run query, send results to callback function
-	connection.pool.query(sql, params, (err, result) => {
-		if (err) {
-			console.log(err);
-			callback(err, null);
+	connection.pool.query(sql, params, (error, result) => {
+		if (error) {
+			console.log("--> user-model.js > getUserByEmail");
+			console.log(error);
+
+			// send back error
+			callback(error, null);
 		}
-		console.log(result.rows[0])
+		console.log("--> user-model.js > getUserByEmail");
+		console.log(result.rows);
+
+		// send back results
 		callback(null, result.rows);
 	});
 }
 
 function insertUser(firstName, lastName, userEmail, userPassword, callback) {
+	// set up sql for query
 	let sql = "INSERT INTO public.user (firstname, lastname, useremail, userpassword) VALUES ($1, $2, $3, $4)";
+	
+	// set up sql parameters
 	let params = [firstName, lastName, userEmail, userPassword];
+
+	// run query, send results to callback function
 	connection.pool.query(sql, params, (error, result) => {
 		if (error) {
 			console.log("--> user-model.js > insertUser");
 			console.log(error);
+
+			// send back error and success = false
 			callback(error, {success: false});
 		}
-		db_result = {
-			success: true,
-			list: result.rows
-		}
 		console.log("--> user-model.js > insertUser");
-		console.log(db_result);
-		callback(null, db_result)
+		console.log(result);
+
+		// send back success = true
+		callback(null, {success: true});
 	});
 }
 
