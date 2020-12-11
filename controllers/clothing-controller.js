3ade7e9c63723415f7cf700bb44d71a0a8a45f94
Hannah Rogers <rog19008@byui.edu>
2020-12-11 14:32:21 -0700

    testing

--- controllers/clothing-controller.js
+++ controllers/clothing-controller.js
@@ -13,248 +13,256 @@ function generateOutfit(req, res) {
 	let userId = req.body.userId;
 
 	// get warmRating based on temp
-	var temp = getTemp(userId);
-	let warmRating = getWarmRating(temp);
-
-	const outfit = [];
-
-	clothingModel.getClothesByUserIdAndWarmRating(userId, warmRating, function(error, result) {
-		// check for errors
-		if(error || result == null) {
-			viewParams.title = "Home";
-			viewParams.message = "Error getting outfit. Please try again.";
-			res.render('pages/home', viewParams);
+	getTemp(userId, function(error, temp) {
+		if (error) {
+			console.log(error);
 		}
-		// generate outfit
 		else {
-			let tops = [];
-			let bottoms = [];
-			let outerwear = [];
-			let onepieces = [];
-			let shoes = [];
+			console.log("TEMP --> " + temp);
+			let warmRating = getWarmRating(temp);
 
-			// check season colors for each item and separate
-			// clothing types onto different arrays
-			let date = new Date();
-			let month = date.getMonth() + 1;
-			result.forEach(e => {
-				switch(e.clothingtype) {
-					case "Top":
-						// check if season is fall/winter
-						if (month >= 9 && month <= 12 ) {
-							// add fall/winter colors to list
-							if (checkFallWinterColors(e.clothingcolor)) {
-								tops.push(e);
-							}
+			const outfit = [];
+
+			clothingModel.getClothesByUserIdAndWarmRating(userId, warmRating, function(error, result) {
+				// check for errors
+				if(error || result == null) {
+					viewParams.title = "Home";
+					viewParams.message = "Error getting outfit. Please try again.";
+					res.render('pages/home', viewParams);
+				}
+				// generate outfit
+				else {
+					let tops = [];
+					let bottoms = [];
+					let outerwear = [];
+					let onepieces = [];
+					let shoes = [];
+
+					// check season colors for each item and separate
+					// clothing types onto different arrays
+					let date = new Date();
+					let month = date.getMonth() + 1;
+					result.forEach(e => {
+						switch(e.clothingtype) {
+							case "Top":
+								// check if season is fall/winter
+								if (month >= 9 && month <= 12 ) {
+									// add fall/winter colors to list
+									if (checkFallWinterColors(e.clothingcolor)) {
+										tops.push(e);
+									}
+								}
+								// otherwise season is spring/summer
+								else {
+									// add spring/summer colors to list
+									if (checkSpringSummerColors(e.clothingcolor)) {
+										tops.push(e);
+									}
+								}
+								break;
+							case "Bottom":
+								// check if season is fall/winter
+								if (month >= 9 && month <= 12 ) {
+									// add fall/winter colors to list
+									if (checkFallWinterColors(e.clothingcolor)) {
+										bottoms.push(e);
+									}
+								}
+								// otherwise season is spring/summer
+								else {
+									// add spring/summer colors to list
+									if (checkSpringSummerColors(e.clothingcolor)) {
+										bottoms.push(e);
+									}
+								}
+								break;
+							case "Outerwear":
+								// check if season is fall/winter
+								if (month >= 9 && month <= 12 ) {
+									// add fall/winter colors to list
+									if (checkFallWinterColors(e.clothingcolor)) {
+										outerwear.push(e);
+									}
+								}
+								// otherwise season is spring/summer
+								else {
+									// add spring/summer colors to list
+									if (checkSpringSummerColors(e.clothingcolor)) {
+										outerwear.push(e);
+									}
+								}
+								break;
+							case "Shoes":
+								// check if season is fall/winter
+								if (month >= 9 && month <= 12 ) {
+									// add fall/winter colors to list
+									if (checkFallWinterColors(e.clothingcolor)) {
+										shoes.push(e);
+									}
+								}
+								// otherwise season is spring/summer
+								else {
+									// add spring/summer colors to list
+									if (checkSpringSummerColors(e.clothingcolor)) {
+										shoes.push(e);
+									}
+								}
+								break;
+							case "One Piece":
+								// check if season is fall/winter
+								if (month >= 9 && month <= 12 ) {
+									// add fall/winter colors to list
+									if (checkFallWinterColors(e.clothingcolor)) {
+										onepieces.push(e);
+									}
+								}
+								// otherwise season is spring/summer
+								else {
+									// add spring/summer colors to list
+									if (checkSpringSummerColors(e.clothingcolor)) {
+										onepieces.push(e);
+									}
+								}
+								break;
 						}
-						// otherwise season is spring/summer
-						else {
-							// add spring/summer colors to list
-							if (checkSpringSummerColors(e.clothingcolor)) {
-								tops.push(e);
+					});
+					
+					// generate random number
+					let int = library.getRandomInt(2);
+
+					// compare casual ratings and
+					// add pieces to outfit array
+					switch (int) {
+						case 0:
+							if (tops.length != 0 && bottoms.length != 0) {
+								// get random top
+								let iTop = library.getRandomInt(tops.length);
+								let top = tops[iTop];
+
+								let tCasual = top.casualrating;
+								outfit.push(top);
+
+								// get random bottom
+								let bottom = compareRating(bottoms, tCasual);
+								if(bottom != undefined) {
+									outfit.push(bottom);
+								}
+
+								// if temp < 60 and items are on array, add outerwear
+								if (temp < 60 && outerwear.length != 0) {
+									let outer = compareRating(outerwear, tCasual);
+									if(outer != undefined) {
+										outfit.push(outer);
+									}
+								}
+
+								// if shoes not empty add shoes with same casual rating
+								if (shoes.length != 0) {
+									let shoe = compareRating(shoes, tCasual);
+									if(shoe != undefined) {
+										outfit.push(shoe);
+									}
+								}
 							}
-						}
+							else if (onepieces.length != 0) {
+								// get random onepiece
+								let iOne = library.getRandomInt(onepieces.length);
+								let onepiece = onepieces[iOne];
+
+								let oCasual = onepiece.casualrating;
+								outfit.push(onepiece);
+
+								// if temp < 60 and items are on array, add outerwear
+								if (temp < 60 && outerwear.length != 0) {
+									let outer = compareRating(outerwear, oCasual);
+									if(outer != undefined) {
+										outfit.push(outer);
+									}
+								}
+
+								// if shoes not empty add shoes with same casual rating
+								if (shoes.length != 0) {
+									let shoe = compareRating(shoes, oCasual);
+									if(shoe != undefined) {
+										outfit.push(shoe);
+									}
+								}
+							}
 						break;
-					case "Bottom":
-						// check if season is fall/winter
-						if (month >= 9 && month <= 12 ) {
-							// add fall/winter colors to list
-							if (checkFallWinterColors(e.clothingcolor)) {
-								bottoms.push(e);
+
+						case 1:
+							if (onepieces.length != 0) {
+								// get random onepiece
+								let iOne = library.getRandomInt(onepieces.length);
+								let onepiece = onepieces[iOne];
+
+								let oCasual = onepiece.casualrating;
+								outfit.push(onepiece);
+
+								// if temp < 60 and items are on array, add outerwear
+								if (temp < 60 && outerwear.length != 0) {
+									let outer = compareRating(outerwear, oCasual);
+									if(outer != undefined) {
+										outfit.push(outer);
+									}
+								}
+
+								// if shoes not empty add shoes with same casual rating
+								if (shoes.length != 0) {
+									let shoe = compareRating(shoes, oCasual);
+									if(shoe != undefined) {
+										outfit.push(shoe);
+									}
+								}
 							}
-						}
-						// otherwise season is spring/summer
-						else {
-							// add spring/summer colors to list
-							if (checkSpringSummerColors(e.clothingcolor)) {
-								bottoms.push(e);
+							else if (tops.length != 0 && bottoms.length != 0) {
+								// get random top
+								let iTop = library.getRandomInt(tops.length);
+								let top = tops[iTop];
+
+								let tCasual = top.casualrating;
+								outfit.push(top);
+
+								// get random bottom
+								let bottom = compareRating(bottoms, tCasual);
+								if(bottom != undefined) {
+									outfit.push(bottom);
+								}
+
+								// if temp < 60 and items are on array, add outerwear
+								if (temp < 60 && outerwear.length != 0) {
+									let outer = compareRating(outerwear, tCasual);
+									if(outer != undefined) {
+										outfit.push(outer);
+									}
+								}
+
+								// if shoes not empty add shoes with same casual rating
+								if (shoes.length != 0) {
+									let shoe = compareRating(shoes, tCasual);
+									if(shoe != undefined) {
+										outfit.push(shoe);
+									}
+								}
 							}
-						}
 						break;
-					case "Outerwear":
-						// check if season is fall/winter
-						if (month >= 9 && month <= 12 ) {
-							// add fall/winter colors to list
-							if (checkFallWinterColors(e.clothingcolor)) {
-								outerwear.push(e);
-							}
-						}
-						// otherwise season is spring/summer
-						else {
-							// add spring/summer colors to list
-							if (checkSpringSummerColors(e.clothingcolor)) {
-								outerwear.push(e);
-							}
-						}
-						break;
-					case "Shoes":
-						// check if season is fall/winter
-						if (month >= 9 && month <= 12 ) {
-							// add fall/winter colors to list
-							if (checkFallWinterColors(e.clothingcolor)) {
-								shoes.push(e);
-							}
-						}
-						// otherwise season is spring/summer
-						else {
-							// add spring/summer colors to list
-							if (checkSpringSummerColors(e.clothingcolor)) {
-								shoes.push(e);
-							}
-						}
-						break;
-					case "One Piece":
-						// check if season is fall/winter
-						if (month >= 9 && month <= 12 ) {
-							// add fall/winter colors to list
-							if (checkFallWinterColors(e.clothingcolor)) {
-								onepieces.push(e);
-							}
-						}
-						// otherwise season is spring/summer
-						else {
-							// add spring/summer colors to list
-							if (checkSpringSummerColors(e.clothingcolor)) {
-								onepieces.push(e);
-							}
-						}
-						break;
+					}	
+					
+					// send outfit and temp
+					res.json({"outfit" : outfit, "temp" : temp});
 				}
 			});
-			
-			// generate random number
-			let int = library.getRandomInt(2);
-
-			// compare casual ratings and
-			// add pieces to outfit array
-			switch (int) {
-				case 0:
-					if (tops.length != 0 && bottoms.length != 0) {
-						// get random top
-						let iTop = library.getRandomInt(tops.length);
-						let top = tops[iTop];
-
-						let tCasual = top.casualrating;
-						outfit.push(top);
-
-						// get random bottom
-						let bottom = compareRating(bottoms, tCasual);
-						if(bottom != undefined) {
-							outfit.push(bottom);
-						}
-
-						// if temp < 60 and items are on array, add outerwear
-						if (temp < 60 && outerwear.length != 0) {
-							let outer = compareRating(outerwear, tCasual);
-							if(outer != undefined) {
-								outfit.push(outer);
-							}
-						}
-
-						// if shoes not empty add shoes with same casual rating
-						if (shoes.length != 0) {
-							let shoe = compareRating(shoes, tCasual);
-							if(shoe != undefined) {
-								outfit.push(shoe);
-							}
-						}
-					}
-					else if (onepieces.length != 0) {
-						// get random onepiece
-						let iOne = library.getRandomInt(onepieces.length);
-						let onepiece = onepieces[iOne];
-
-						let oCasual = onepiece.casualrating;
-						outfit.push(onepiece);
-
-						// if temp < 60 and items are on array, add outerwear
-						if (temp < 60 && outerwear.length != 0) {
-							let outer = compareRating(outerwear, oCasual);
-							if(outer != undefined) {
-								outfit.push(outer);
-							}
-						}
-
-						// if shoes not empty add shoes with same casual rating
-						if (shoes.length != 0) {
-							let shoe = compareRating(shoes, oCasual);
-							if(shoe != undefined) {
-								outfit.push(shoe);
-							}
-						}
-					}
-				break;
-
-				case 1:
-					if (onepieces.length != 0) {
-						// get random onepiece
-						let iOne = library.getRandomInt(onepieces.length);
-						let onepiece = onepieces[iOne];
-
-						let oCasual = onepiece.casualrating;
-						outfit.push(onepiece);
-
-						// if temp < 60 and items are on array, add outerwear
-						if (temp < 60 && outerwear.length != 0) {
-							let outer = compareRating(outerwear, oCasual);
-							if(outer != undefined) {
-								outfit.push(outer);
-							}
-						}
-
-						// if shoes not empty add shoes with same casual rating
-						if (shoes.length != 0) {
-							let shoe = compareRating(shoes, oCasual);
-							if(shoe != undefined) {
-								outfit.push(shoe);
-							}
-						}
-					}
-					else if (tops.length != 0 && bottoms.length != 0) {
-						// get random top
-						let iTop = library.getRandomInt(tops.length);
-						let top = tops[iTop];
-
-						let tCasual = top.casualrating;
-						outfit.push(top);
-
-						// get random bottom
-						let bottom = compareRating(bottoms, tCasual);
-						if(bottom != undefined) {
-							outfit.push(bottom);
-						}
-
-						// if temp < 60 and items are on array, add outerwear
-						if (temp < 60 && outerwear.length != 0) {
-							let outer = compareRating(outerwear, tCasual);
-							if(outer != undefined) {
-								outfit.push(outer);
-							}
-						}
-
-						// if shoes not empty add shoes with same casual rating
-						if (shoes.length != 0) {
-							let shoe = compareRating(shoes, tCasual);
-							if(shoe != undefined) {
-								outfit.push(shoe);
-							}
-						}
-					}
-				break;
-			}	
-			
-			// send outfit and temp
-			res.json({"outfit" : outfit, "temp" : temp});
 		}
 	});
+	
 }
 
-function getTemp(userId) {
+function getTemp(userId, callback) {
 	/* Uses OpenWeather API to return high temp as an int */
 	userModel.getZipCodeByUserId(userId, function(error, result) {
 		if (error || result == undefined) {
 			console.error('There was a problem getting the zip code: ', error);
-			return 60;
+			callback(error, 60);
 		}
 		console.log(result);
 		let url = "https://api.openweathermap.org/data/2.5/weather?zip=" + result + ",us&units=imperial&" + "APPID=29532d73c747db9a9cb1be86effaaac6";
@@ -263,15 +271,15 @@ function getTemp(userId) {
         .then((response) => response.json())
         .then((jsObject) => {
 			if (jsObject != undefined) {
-				return Math.round(jsObject.main.temp_max);
+				callback(null, Math.round(jsObject.main.temp_max));
 			}
 			else {
-				return 60;
+				callback("response undefined", 60);
 			}
         })
         .catch(error => {
             console.error('There was a problem getting the temp: ', error);
-            return 60;
+            callback(error, 60);
         });
 	});
 }
