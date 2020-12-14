fce2d64ef2d64f397de534463d09e0991af1e178
Hannah Rogers <rog19008@byui.edu>
2020-12-14 15:37:15 -0700

    testing

--- public/scripts/message.js
+++ public/scripts/message.js
@@ -4,7 +4,22 @@ $(document).ready(function() {
     if (message.html() == "" || message.html() == undefined) {
         message.css("display", "none");
     }
-    console.log(message.html());
+
+    if (message.html() == "Item successfully added." 
+    || message.html() == "Item successfully updated."
+    || message.html() == "Item successfully deleted.") {
+        // success class
+        message.addClass("success");
+    }
+
+    if (message.html() == "Item was not added, please try again." 
+    || message.html() == "Item was not updated, please try again."
+    || message.html() == "Item was not deleted, please try again."
+    || message.html() == "Could not get information for item. Please try again.") {
+        // warning class
+        message.addClass("warning");
+    }
+
     setTimeout(function() {
         message.fadeOut();    
     }, 10000);
