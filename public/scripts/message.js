9adba4862dac4b193f87b175131b75fff3949d0d
Hannah Rogers <rog19008@byui.edu>
2020-12-14 15:26:10 -0700

    testing

--- public/scripts/message.js
+++ public/scripts/message.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-    console.log(message.html());
+    
     var message = $('#message_fade');
     if (message.html() == "" || message.html() == undefined) {
         message.css("display", "none");
     }
-    
+    console.log(message.html());
     setTimeout(function() {
         message.fadeOut();    
     }, 10000);
